eb6cf7200211ecd3d53aa8cfa99dd7e78ed81648
takwaaouichaoui <99695491+takwaaouichaoui@users.noreply.github.com>
2023-09-26 21:01:05 +0200

    Delete WS directory

--- WS/GraphQLTest-main/src/main/java/entite/Type.java
+++
@@ -1,5 +0,0 @@
-package entite;
-
-public enum Type {
-	Studio, Appartement, Villa, EtageVilla
-};
--- WS/JwtWebSevice-main/src/main/java/jaxrs/utilities/RestActivator.java
+++
@@ -1,9 +0,0 @@
-package jaxrs.utilities;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("sec-rest")
-public class RestActivator extends Application {
-
-}
